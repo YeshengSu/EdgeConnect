ed188395cd5cd96de878e4e4894e7f8a219ec9ee
suyesheng <1245035229@qq.com>
2020-01-11 23:53:03 +0800

    optimised codes

--- image_process.py
+++ image_process.py
@@ -2,39 +2,62 @@
 import os
 
 import PIL
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageFilter
 import numpy as np
 
 # 待处理图片地址
-dataPath = r'E:\DeepLearnSample\EdgeConnect\mask\qd_imd\train'
+data_path = r'E:\DeepLearnSample\EdgeConnect\mask\qd_imd\test'
 # 保存图片的地址
-savePath = r'E:\DeepLearnSample\EdgeConnect\mask\CustomMask3\train'
+save_path = r'E:\DeepLearnSample\EdgeConnect\mask\CustomMask3\test'
 
 
-def invert_color(imgPath, savePath):
+def image_resize(new_img, size):
+    new_img = new_img.resize(size, Image.ANTIALIAS)  # 改变图片大小
+    return new_img
+
+
+def image_invert_color(new_img):
+    matrix = np.asarray(new_img)  # 图像转矩阵
+    matrix = 255 - matrix  # 图像转矩阵 并反色
+    new_img = Image.fromarray(matrix)  # 矩阵转图像
+    # new_img = ImageOps.invert(new_img)  # 反色
+    return new_img
+
+
+def image_save(new_img, savePath, image_name, image_type=None):
+    if image_type:
+        image_type = image_type  # 保存的图片格式
+        image_file = image_name.split('.', 1)[0] + '.' + image_type  # 保存的图片名字
+        new_img.save(savePath + '/' + image_file, image_type)  # 保存图片
+        return savePath + '/' + image_file
+    else:
+        new_img.save(savePath + '/' + image_name)  # 保存图片
+        return savePath + '/' + image_name
+
+
+def image_process(imgPath, savePath):
     files = os.listdir(imgPath)
     files.sort()
     print('Data Path :', imgPath)
     print('Save Path :', savePath)
     print('start...')
+
+    img_num = len(files)
     for index, file in enumerate(files):
+        new_img = Image.open(imgPath + '/' + file)  # 打开图片
+        # new_img = image_resize(new_img, (1024, 1024)) # 改变图片大小
+        # new_img = new_img.convert('RGB')  # 改变格式
+        new_img = new_img.convert('L')  # 改变格式
+        # new_img = new_img.filter(ImageFilter.BLUR)  # 高斯模糊
+        new_img = image_invert_color(new_img)
+        new_img = new_img.convert('1')  # 改变格式 只存在黑白
 
-        new_png = Image.open(imgPath + '/' + file)  # 打开图片
-        # new_png = new_png.resize((20, 20),Image.ANTIALIAS) # 改变图片大小
+        image_path = image_save(new_img, savePath, file, 'png')
 
-        new_png = new_png.convert('L')  # 改变格式
-        new_png = ImageOps.invert(new_png)  # 反色
-        new_png = new_png.convert('1')  # 变为位图格式
+        print(index+1, '/', img_num, '  process image : ', imgPath + '/' + file, '  save image : ', image_path)
 
-        # matrix = np.asarray(new_png)  # 图像转矩阵
-        # matrix = 255-matrix  # 图像转矩阵 并反色
-        # new_png = Image.fromarray(matrix)  # 矩阵转图像
-
-        new_png.save(savePath + '/' + file)  # 保存图片
-
-        print('num', index, 'process image : ', imgPath + '\\' + file, '    save image : ', savePath + '\\' + file)
     print('done!')
 
 
 if __name__ == '__main__':
-    invert_color(dataPath, savePath)
+    image_process(data_path, save_path)
