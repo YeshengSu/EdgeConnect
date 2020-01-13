7a6a522fa4341d8f6a0f67a1352c9568b0b3f8b0
EASY <1245035229@qq.com>
2020-01-14 02:26:15 +0800

    finish file explorer

--- scripts/image_inpainting.py
+++ scripts/image_inpainting.py
@@ -1,120 +1,173 @@
+import os
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QWidget, QTreeWidget, QTreeWidgetItem, QLabel, QHBoxLayout, QToolBox, \
-    QToolButton, QGroupBox, QVBoxLayout
+import numpy as np
+from collections import defaultdict
 
+from PIL import Image
+from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtGui import QIcon, QImage, QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QTreeWidget, QTreeWidgetItem, QLabel, QHBoxLayout, QToolBox, \
+    QToolButton, QGroupBox, QVBoxLayout, QTabWidget, QPushButton, QGridLayout, QFileDialog, QMessageBox
+
+import utils
 
 class FileExplorer(QWidget):
+    preview_width = 450
+
     def __init__(self, parent=None):
         super(FileExplorer, self).__init__(parent=parent)
-        self.resize(500, 500)
-        self.label = QLabel('No Click')                         # 1
 
-        self.tree = QTreeWidget(self)                           # 2
+        self.new_folder_path = ''
+        self.all_folders = defaultdict(list)
+        self.click_item = None
+        self.select_image = None
+        self.image_data = None
+
+        self.button_add_folder = QPushButton('Add Folder', self)
+        self.button_add_folder.clicked.connect(self.on_clicked_add_folder)
+        self.button_remove_folder = QPushButton('Remove Folder', self)
+        self.button_remove_folder.clicked.connect(self.on_clicked_remove_folder)
+
+        self.tree = QTreeWidget(self)
         self.tree.setColumnCount(2)
         self.tree.setHeaderLabels(['Files', 'Format'])
-        self.tree.itemClicked.connect(self.change_func)
+        self.tree.setColumnWidth(0, 350)
+        self.tree.setMaximumWidth(500)
+        self.tree.itemClicked.connect(self.on_clicked_tree_item)
 
-        self.preview = QTreeWidgetItem(self.tree)               # 3
+        self.preview = QTreeWidgetItem(self.tree)
         self.preview.setText(0, 'ALL Folders')
 
-        self.qt5112 = QTreeWidgetItem(self.preview)                         # 4
-        self.qt5112.setText(0, 'Qt 5.11.2 snapshot')
-        self.qt5112.setText(1, 'Folder')
+        self.image_preview = QLabel('No Info')
 
-        choice_list = ['macOS', 'Android x86', 'Android ARMv7', 'Sources', 'iOS']
-        self.item_list = []
-        for i, c in enumerate(choice_list):                     # 5
-            item = QTreeWidgetItem(self.qt5112)
-            item.setText(0, c)
-            item.setText(1, 'Image')
-            item.setCheckState(0, Qt.Unchecked)
-            self.item_list.append(item)
+        self.hbox_layout = QHBoxLayout()
+        self.hbox_layout.addWidget(self.button_add_folder)
+        self.hbox_layout.addWidget(self.button_remove_folder)
+        self.grid_layout = QGridLayout()
+        self.grid_layout.addLayout(self.hbox_layout, 0, 0, 1, 1)
+        self.grid_layout.addWidget(self.tree, 1, 0, 5, 5)
+        self.grid_layout.addWidget(self.image_preview, 1, 5, 5, 5)
+        self.setLayout(self.grid_layout)
 
-        self.tree.expandAll()                                   # 7
+        self.tree.expandAll()
 
+    def on_clicked_tree_item(self, item, column):
+        self.image_preview.setText(item.text(column))
+        self.click_item = item
+
+        # is select image
+        if item.checkState(0) == Qt.Checked:
+            if self.select_image and self.select_image is not item:
+                self.select_image.setCheckState(0, Qt.Unchecked)
+            self.select_image = item
+            self.select_image.setCheckState(0, Qt.Checked)
+
+            image_path = self.select_image.parent().text(0) + '/' + self.select_image.text(0)
+            print('select image path : ', image_path)
+            # preview image
+            new_img = Image.open(image_path)
+            new_img = new_img.convert('RGB')
+            self.image_data = np.asarray(new_img)
+            preview_height = int(self.preview_width * self.image_data.shape[0] / self.image_data.shape[1])
+            new_img = np.array(Image.fromarray(self.image_data).resize((self.preview_width, preview_height), Image.BOX))
+            height, width, bytesPerComponent = new_img.shape
+            QImg = QImage(new_img, width, height, width*3, QImage.Format_RGB888)
+            pixmap = QPixmap.fromImage(QImg)
+            self.image_preview.setPixmap(pixmap)
+            # todo: emit select image
+            # self.label.setCursor(Qt.CrossCursor)
+
+    def on_clicked_add_folder(self):
+        self.new_folder_path = QFileDialog.getExistingDirectory(self, "Add Folder", self.new_folder_path)
+        if self.new_folder_path:
+            print('add new folder path : ', self.new_folder_path)
+
+            # add folder item
+            folder_item = QTreeWidgetItem(self.preview)
+            folder_item.setText(0, self.new_folder_path)
+            folder_item.setText(1, 'Folder')
+
+            images = os.listdir(self.new_folder_path)
+
+            # filter images
+            def is_image(path):
+                suffix = path.split('.')[-1]
+                if suffix in ('jpg', 'png', 'bmp', 'jpeg'):
+                    return True
+                return False
+
+            images = list(filter(is_image, images))
+            images.sort()
+            self.all_folders[self.new_folder_path] = images
+
+            # add image item
+            for i, image in enumerate(images):
+                item = QTreeWidgetItem(folder_item)
+                item.setText(0, image)
+                item.setText(1, 'Image')
+                item.setCheckState(0, Qt.Unchecked)
+
+    def on_clicked_remove_folder(self):
+        if self.click_item:
+            if self.click_item.text(1) == 'Folder':
+                if self.click_item.text(0) in self.all_folders:
+                    del self.all_folders[self.click_item.text(0)]
+                    self.preview.removeChild(self.click_item)
+                    self.click_item = None
+            else:
+                QMessageBox.warning(self, 'warning', 'only allow to remove folder')
+
+
+class SelectArea(QWidget):
+    my_signal = pyqtSignal()
+
+    def __init__(self, parent=None):
+        super(SelectArea, self).__init__(parent=parent)
+        self.my_signal.connect(self.select_image)
+
+        self.label = QLabel('SelectArea')
         self.h_layout = QHBoxLayout()
-        self.h_layout.addWidget(self.tree)
         self.h_layout.addWidget(self.label)
         self.setLayout(self.h_layout)
 
-    def change_func(self, item, column):
-        self.label.setText(item.text(column))                   # 8
-
-        print(item.text(column))
-        print(column)
-        # 10
-        check_count = 0
-        for x in self.item_list:
-            if x.checkState(0) == Qt.Checked:
-                check_count += 1
+    def select_image(self, image_path):
+        print(image_path)
 
 
-class FunctionTag(QToolBox):                                           # 1
-    def __init__(self):
-        super(FunctionTag, self).__init__()
-        self.groupbox_1 = QGroupBox(self)                       # 2
-        self.groupbox_2 = QGroupBox(self)
-        self.groupbox_3 = QGroupBox(self)
+class DrawMask(QWidget):
+    def __init__(self, parent=None):
+        super(DrawMask, self).__init__(parent=parent)
+        self.label = QLabel('DrawMask')
+        self.h_layout = QHBoxLayout()
+        self.h_layout.addWidget(self.label)
+        self.setLayout(self.h_layout)
 
-        self.file_explorer = FileExplorer(self)
-        self.toolbtn_f1 = QToolButton(self)                     # 3
-        self.toolbtn_f2 = QToolButton(self)
-        self.toolbtn_f3 = QToolButton(self)
-        self.toolbtn_m1 = QToolButton(self)
-        self.toolbtn_m2 = QToolButton(self)
-        self.toolbtn_m3 = QToolButton(self)
 
-        self.v1_layout = QVBoxLayout()
-        self.v2_layout = QVBoxLayout()
-        self.v3_layout = QVBoxLayout()
+class ImageInpainting(QWidget):
+    def __init__(self, parent=None):
+        super(ImageInpainting, self).__init__(parent=parent)
+        self.label = QLabel('ImageInpainting')
+        self.h_layout = QHBoxLayout()
+        self.h_layout.addWidget(self.label)
+        self.setLayout(self.h_layout)
 
-        self.addItem(self.groupbox_1, 'Couple One')             # 4
-        self.addItem(self.groupbox_2, 'Couple Two')
-        self.addItem(self.groupbox_3, 'Couple Three')
-        self.currentChanged.connect(self.print_index_func)      # 5
 
-        self.layout_init()
-        self.groupbox_init()
-        self.toolbtn_init()
+class MainWindow(QTabWidget):
+    # my_signal = pyqtSignal()
 
-    def layout_init(self):
-        self.v1_layout.addWidget(self.file_explorer)
-        self.v1_layout.addWidget(self.toolbtn_f1)
-        self.v1_layout.addWidget(self.toolbtn_m1)
-        self.v2_layout.addWidget(self.toolbtn_f2)
-        self.v2_layout.addWidget(self.toolbtn_m2)
-        self.v3_layout.addWidget(self.toolbtn_f3)
-        self.v3_layout.addWidget(self.toolbtn_m3)
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent=parent)
 
-    def groupbox_init(self):                                    # 6
-        self.groupbox_1.setFlat(True)
-        self.groupbox_2.setFlat(True)
-        self.groupbox_3.setFlat(True)
-        self.groupbox_1.setLayout(self.v1_layout)
-        self.groupbox_2.setLayout(self.v2_layout)
-        self.groupbox_3.setLayout(self.v3_layout)
+        self.setWindowTitle('Edge Connect')
+        self.resize(1000, 800)
+        self.addTab(FileExplorer(self), 'File Explorer')
+        self.addTab(SelectArea(self), 'Select Area')
+        self.addTab(DrawMask(self), 'Draw Mask')
+        self.addTab(ImageInpainting(self), 'Image Inpainting')
 
-    def toolbtn_init(self):                                     # 7
-        self.toolbtn_f1.setIcon(QIcon('images/f1.ico'))
-        self.toolbtn_f2.setIcon(QIcon('images/f2.ico'))
-        self.toolbtn_f3.setIcon(QIcon('images/f3.ico'))
-        self.toolbtn_m1.setIcon(QIcon('images/m1.ico'))
-        self.toolbtn_m2.setIcon(QIcon('images/m2.ico'))
-        self.toolbtn_m3.setIcon(QIcon('images/m3.ico'))
-
-    def print_index_func(self):
-        couple_dict = {
-            0: 'Couple One',
-            1: 'Couple Two',
-            2: 'Couple Three'
-        }
-        sentence = 'You are looking at {}.'.format(couple_dict.get(self.currentIndex()))
-        print(sentence)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    demo = FunctionTag()
+    demo = MainWindow()
     demo.show()
     sys.exit(app.exec_())
