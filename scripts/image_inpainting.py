334992626fdd85e1f290be510a5d7a1660c164c8
suyesheng <1245035229@qq.com>
2020-01-14 20:10:20 +0800

    optimise file explorer

--- scripts/image_inpainting.py
+++ scripts/image_inpainting.py
@@ -5,9 +5,10 @@ from collections import defaultdict
 
 from PIL import Image
 from PyQt5.QtCore import Qt, pyqtSignal
-from PyQt5.QtGui import QIcon, QImage, QPixmap
+from PyQt5.QtGui import QIcon, QImage, QPixmap, QFont
 from PyQt5.QtWidgets import QApplication, QWidget, QTreeWidget, QTreeWidgetItem, QLabel, QHBoxLayout, QToolBox, \
-    QToolButton, QGroupBox, QVBoxLayout, QTabWidget, QPushButton, QGridLayout, QFileDialog, QMessageBox
+    QToolButton, QGroupBox, QVBoxLayout, QTabWidget, QPushButton, QGridLayout, QFileDialog, QMessageBox, QScrollArea, \
+    QScrollBar
 
 import utils
 
@@ -16,6 +17,7 @@ class FileExplorer(QWidget):
 
     def __init__(self, parent=None):
         super(FileExplorer, self).__init__(parent=parent)
+        self.parent = parent
 
         self.new_folder_path = ''
         self.all_folders = defaultdict(list)
@@ -32,13 +34,14 @@ class FileExplorer(QWidget):
         self.tree.setColumnCount(2)
         self.tree.setHeaderLabels(['Files', 'Format'])
         self.tree.setColumnWidth(0, 350)
-        self.tree.setMaximumWidth(500)
+        # self.tree.setMaximumWidth(500)
         self.tree.itemClicked.connect(self.on_clicked_tree_item)
 
         self.preview = QTreeWidgetItem(self.tree)
         self.preview.setText(0, 'ALL Folders')
 
         self.image_preview = QLabel('No Info')
+        self.image_preview.setAlignment(Qt.AlignCenter)
 
         self.hbox_layout = QHBoxLayout()
         self.hbox_layout.addWidget(self.button_add_folder)
@@ -74,8 +77,7 @@ class FileExplorer(QWidget):
             QImg = QImage(resized_img_data, width, height, width*3, QImage.Format_RGB888)
             pixmap = QPixmap.fromImage(QImg)
             self.image_preview.setPixmap(pixmap)
-            # todo: emit select image
-            # self.label.setCursor(Qt.CrossCursor)
+            self.parent.select_area.select_image(self.image_data)
 
     def on_clicked_add_folder(self):
         self.new_folder_path = QFileDialog.getExistingDirectory(self, "Add Folder", self.new_folder_path)
@@ -115,29 +117,61 @@ class FileExplorer(QWidget):
         if self.click_item:
             if self.click_item.text(1) == 'Folder':
                 if self.click_item.text(0) in self.all_folders:
+                    if self.select_image:
+                        self.select_image.setCheckState(0, Qt.Unchecked)
                     del self.all_folders[self.click_item.text(0)]
                     self.preview.removeChild(self.click_item)
                     self.click_item = None
                     self.select_image = None
+                    self.parent.setTabEnabled(1, False)
+                    self.parent.setTabEnabled(2, False)
+                    self.parent.setTabEnabled(3, False)
             else:
                 QMessageBox.warning(self, 'warning', 'only allow to remove folder !')
 
 
 class SelectArea(QWidget):
-    my_signal = pyqtSignal()
+    preview_width = 450
 
     def __init__(self, parent=None):
         super(SelectArea, self).__init__(parent=parent)
-        self.my_signal.connect(self.select_image)
+        self.parent = parent
+        self.image_data = None
 
-        self.label = QLabel('SelectArea')
-        self.h_layout = QHBoxLayout()
-        self.h_layout.addWidget(self.label)
-        self.setLayout(self.h_layout)
+        self.ft = QFont()
+        self.ft.setPointSize(25)
+        self.label_select = QLabel('image show')
+        self.label_select.setAlignment(Qt.AlignCenter)
+        self.label_select.setFont(self.ft)
+        self.label_preview = QLabel('selected area preview')
+        self.label_preview.setAlignment(Qt.AlignCenter)
+        self.label_preview.setFont(self.ft)
 
-    def select_image(self, image_path):
-        print(image_path)
+        self.image_select = QLabel('image show')
+        self.image_select.setAlignment(Qt.AlignCenter)
+        self.image_select.setCursor(Qt.CrossCursor)
+        self.selected_area_preview = QLabel('image select')
+        self.selected_area_preview.setAlignment(Qt.AlignCenter)
 
+        self.grid_layout = QGridLayout()
+        self.grid_layout.addWidget(self.label_select, 0, 2, 1, 1)
+        self.grid_layout.addWidget(self.label_preview, 0, 7, 1, 1)
+        self.grid_layout.addWidget(self.image_select, 1, 0, 5, 5)
+        self.grid_layout.addWidget(self.selected_area_preview, 1, 5, 5, 5)
+
+        self.setLayout(self.grid_layout)
+
+    def select_image(self, image_data):
+        self.image_data = image_data
+        preview_height = int(self.preview_width * self.image_data.shape[0] / self.image_data.shape[1])
+        resized_img_data = np.array(Image.fromarray(self.image_data).resize((self.preview_width, preview_height), Image.HAMMING))
+        height, width, bytesPerComponent = resized_img_data.shape
+        QImg = QImage(resized_img_data, width, height, width * 3, QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(QImg)
+        self.image_select.setPixmap(pixmap)
+        self.selected_area_preview.setPixmap(pixmap)
+
+        self.parent.setTabEnabled(1, True)
 
 class DrawMask(QWidget):
     def __init__(self, parent=None):
@@ -163,12 +197,21 @@ class MainWindow(QTabWidget):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent=parent)
 
+        self.file_explorer = FileExplorer(self)
+        self.select_area = SelectArea(self)
+        self.draw_mask = DrawMask(self)
+        self.image_inpainting = ImageInpainting(self)
+
         self.setWindowTitle('Edge Connect')
         self.resize(1000, 800)
-        self.addTab(FileExplorer(self), 'File Explorer')
-        self.addTab(SelectArea(self), 'Select Area')
-        self.addTab(DrawMask(self), 'Draw Mask')
-        self.addTab(ImageInpainting(self), 'Image Inpainting')
+        self.addTab(self.file_explorer, 'File Explorer')
+        self.addTab(self.select_area, 'Select Area')
+        self.addTab(self.draw_mask, 'Draw Mask')
+        self.addTab(self.image_inpainting, 'Image Inpainting')
+
+        self.setTabEnabled(1, False)
+        self.setTabEnabled(2, False)
+        self.setTabEnabled(3, False)
 
 
 if __name__ == '__main__':
