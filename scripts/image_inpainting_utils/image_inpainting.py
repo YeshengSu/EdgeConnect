a4c4977842d9a7a0f1048d0ac45469167c2fb55a
EASY <1245035229@qq.com>
2020-01-18 22:27:27 +0800

    finish image inpainting

--- scripts/image_inpainting_utils/image_inpainting.py
+++ scripts/image_inpainting_utils/image_inpainting.py
@@ -1,9 +1,187 @@
+import cv2
+from PyQt5.QtCore import Qt
+import glob
+import os
+import random
+
 import math
 import numpy as np
+import torch
+import torchvision.transforms.functional as F
 from PIL import Image
-from PyQt5.QtCore import Qt, QRect
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QWidget, QLabel, QHBoxLayout, QPushButton, QGridLayout, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QGridLayout, QVBoxLayout
+from imageio import imread
+from skimage.color import rgb2gray, gray2rgb
+from skimage.feature import canny
+from torch.utils.data import DataLoader
+
+import utils
+
+
+
+class ImageDataset(torch.utils.data.Dataset):
+    def __init__(self, config, data, edge_data, mask_data, augment=True, training=True):
+        super(ImageDataset, self).__init__()
+        self.mode = config.MODE
+        self.augment = augment
+        self.training = training
+        self.data = data  # 最好为n^2 的不带alpha的RGB图片
+        self.edge_data = edge_data
+        self.mask_data = mask_data  # 必须位图 类型为 Pil的 ‘1’
+
+        self.input_size = config.INPUT_SIZE
+        self.sigma = config.SIGMA
+        self.edge = config.EDGE
+        self.mask = config.MASK
+        self.nms = config.NMS
+
+        # in test mode, there's a one-to-one relationship between mask and image
+        # masks are loaded non random
+        if config.MODE == 2:
+            self.mask = 6
+
+        # preventing inaccurate ratio of img in train and losing details in test
+        self.is_center_crop = True if self.mode == 1 else False
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        try:
+            item = self.load_item(index)
+        except:
+            print('loading error: index : {}'.format([index]))
+            item = self.load_item(0)
+
+        return item
+
+    def load_item(self, index):
+        size = self.input_size
+
+        # load image
+        ori_img = self.data[index]
+
+        # gray to rgb
+        if len(ori_img.shape) < 3:
+            ori_img = gray2rgb(ori_img)
+
+        # resize/crop if needed
+        if size != 0:
+            img = utils.resize(ori_img, size, size, self.is_center_crop)
+        else:
+            max_size = max(ori_img.shape[0], ori_img.shape[1])
+            max_size = 2 ** math.ceil(math.log2(max_size))
+            img = utils.resize(ori_img, max_size, max_size, self.is_center_crop)
+
+        # create grayscale image
+        img_gray = rgb2gray(img)
+
+        # load mask
+        mask = self.load_mask(img, index)
+
+        # resize/crop if needed
+        if size == 0:
+            img_shape = img.shape
+            mask = utils.resize(mask, img_shape[0], img_shape[1], self.is_center_crop)
+
+        # load edge
+        edge = self.load_edge(img_gray, index, mask)
+
+        # augment data
+        if self.augment and np.random.binomial(1, 0.5) > 0:
+            img = img[:, ::-1, ...]
+            img_gray = img_gray[:, ::-1, ...]
+            edge = edge[:, ::-1, ...]
+            mask = mask[:, ::-1, ...]
+
+        return list(ori_img.shape), self.to_tensor(img), self.to_tensor(img_gray), self.to_tensor(edge), self.to_tensor(mask)
+
+    def load_edge(self, img, index, mask):
+        sigma = self.sigma
+
+        # in test mode images are masked (with masked regions),
+        # using 'mask' parameter prevents canny to detect edges for the masked regions
+        mask = None if self.training else (1 - mask / 255).astype(np.bool)
+
+        # canny
+        if self.edge == 1:
+            # no edge
+            if sigma == -1:
+                return np.zeros(img.shape).astype(np.float)
+
+            # random sigma
+            if sigma == 0:
+                sigma = random.randint(1, 4)
+
+            return canny(img, sigma=sigma, mask=mask).astype(np.float)
+
+        # external
+        else:
+            imgh, imgw = img.shape[0:2]
+            edge = self.edge_data[index]
+            edge = utils.resize(edge, imgh, imgw, self.is_center_crop)
+
+            # non-max suppression
+            if self.nms == 1:
+                edge = edge * canny(img, sigma=sigma, mask=mask)
+
+            return edge
+
+    def load_mask(self, img, index):
+        imgh, imgw = img.shape[0:2]
+        mask_type = self.mask
+
+        # external + random block
+        if mask_type == 4:
+            mask_type = 1 if np.random.binomial(1, 0.5) == 1 else 3
+
+        # external + random block + half
+        elif mask_type == 5:
+            mask_type = np.random.randint(1, 4)
+
+        # random block
+        if mask_type == 1:
+            return utils.create_mask(imgw, imgh, imgw // 2, imgh // 2)
+
+        # half
+        if mask_type == 2:
+            # randomly choose right or left
+            return utils.create_mask(imgw, imgh, imgw // 2, imgh, 0 if random.random() < 0.5 else imgw // 2, 0)
+
+        # external
+        if mask_type == 3:
+            mask_index = random.randint(0, len(self.mask_data) - 1)
+            mask = self.mask_data[mask_index]
+            mask = utils.resize(mask, imgh, imgw, self.is_center_crop)
+            mask = (mask > 0).astype(np.uint8) * 255       # threshold due to interpolation
+            return mask
+
+        # test mode: load mask non random
+        if mask_type == 6:
+            mask = self.mask_data[index%len(self.mask_data)]
+            mask = utils.resize(mask, imgh, imgw, centerCrop=False)
+            mask = rgb2gray(mask)
+            mask = (mask > 0).astype(np.uint8) * 255
+            return mask
+
+    def to_tensor(self, img):
+        img = Image.fromarray(img)
+        img_t = F.to_tensor(img).float()
+        return img_t
+
+    def create_iterator(self, batch_size):
+        while True:
+            sample_loader = DataLoader(
+                dataset=self,
+                batch_size=batch_size,
+                drop_last=True
+            )
+
+            for item in sample_loader:
+                yield item
+
 
 
 class ImageInpainting(QWidget):
@@ -13,7 +191,8 @@ class ImageInpainting(QWidget):
     PREVIEW_RESULT_WIDTH = 450
 
     INFO_READY = 'Info : READY'
-    INFO_PROCESS = 'Info : Processing - {} %'
+    INFO_PROCESS = 'Info : Processing....'
+    INFO_FINSH = 'Info : ACCOMPLISHMENT !'
 
     def __init__(self, parent=None):
         super(ImageInpainting, self).__init__(parent=parent)
@@ -93,7 +272,8 @@ class ImageInpainting(QWidget):
     def on_clicked_start(self):
         print('on_click_start')
         self.btn_start.setEnabled(False)
-        self.label_info.setText(self.INFO_PROCESS.format(0))
+        self.btn_show.setEnabled(False)
+        self.label_info.setText(self.INFO_PROCESS)
         self.image_inpainting()
 
     def on_clicked_show(self):
@@ -107,4 +287,77 @@ class ImageInpainting(QWidget):
         row_img.show('inpainted image')
 
     def image_inpainting(self):
-        pass
+        from image_inpainting_demo import set_label_image
+        from main import load_config
+        from models import EdgeModel
+        from models import InpaintingModel
+
+        mode = 2  # test
+
+        config = load_config(mode)  # start test
+
+        # cuda visble devices
+        os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(str(e) for e in config.GPU)
+
+        # init device
+        if torch.cuda.is_available():
+            config.DEVICE = torch.device("cuda")
+            torch.backends.cudnn.benchmark = True  # cudnn auto-tuner
+        else:
+            config.DEVICE = torch.device("cpu")
+
+        # set cv2 running threads to 1 (prevents deadlocks with pytorch dataloader)
+        cv2.setNumThreads(0)
+
+        # initialize random seed
+        torch.manual_seed(config.SEED)
+        torch.cuda.manual_seed_all(config.SEED)
+        np.random.seed(config.SEED)
+        random.seed(config.SEED)
+
+        # init edge connect
+        model_name = 'edge_inpaint'
+        edge_model = EdgeModel(config).to(config.DEVICE)
+        inpaint_model = InpaintingModel(config).to(config.DEVICE)
+
+        # load dataset
+        test_dataset = ImageDataset(config, [self.image_clip_data], [], [self.image_mask_data],
+                                    augment=False, training=False)
+
+        # load model
+        edge_model.load()
+        inpaint_model.load()
+
+        # start testing
+        print('\nstart testing...\n')
+
+        edge_model.eval()
+        inpaint_model.eval()
+        test_loader = DataLoader(dataset=test_dataset, batch_size=1)
+
+        for items in test_loader:
+            ori_shapes, images, images_gray, edges, masks = self.cuda(*items)
+            edges = self.edge_model(images_gray, edges, masks).detach()
+            outputs = self.inpaint_model(images, edges, masks)
+            outputs_merged = (outputs * masks) + (images * (1 - masks))
+
+            # postprocess
+            outputs_merged = outputs_merged * 255.0
+            outputs_merged = outputs_merged.permute(0, 2, 3, 1)
+            output = outputs_merged.int()
+
+            # output result img
+            ori_shapes = [t.item() for t in ori_shapes]
+            image = output.cpu().numpy().astype(np.uint8)
+            image = utils.resize(image, ori_shapes[0], ori_shapes[1], False)
+            image_result_data = image.squeeze()
+
+            self.image_result_data = image_result_data
+            set_label_image(self.label_image_result, self.PREVIEW_RESULT_WIDTH, self.image_result_data)
+
+            break
+        print('\nEnd test....')
+
+        self.btn_start.setEnabled(True)
+        self.btn_show.setEnabled(True)
+        self.label_info.setText(self.INFO_FINSH)
