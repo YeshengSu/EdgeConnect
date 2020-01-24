ca40a1a7269988b451ea303213638e6903e704ce
suyesheng <1245035229@qq.com>
2020-01-24 14:04:48 +0800

    optimise codes

--- scripts/draw_graph.py
+++ scripts/draw_graph.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-dege_log_data = {'path': r'../checkpoints/log_edge.dat', 'data': 5, 'name': 'precision', 'range': (0.0, 1.0)}
+dege_log_data = {'path': r'../checkpoints/log_edge_inpaint.dat', 'data': 7, 'name': 'psnr', 'range': (15.0, 26.0)}
 
 
 def preprocess(coord_x_list, coord_y_list, multiple):
@@ -25,7 +25,8 @@ def preprocess(coord_x_list, coord_y_list, multiple):
     return new_coord_x_list, new_coord_y_list
 
 
-def read_files(log_data, preprocess):
+def read_files(log_data):
+    coord_x_lists, coord_y_lists = [], []
     with open(log_data['path'], "r") as f:
         datas = f.readlines()
         coord_x_list = []
@@ -35,20 +36,34 @@ def read_files(log_data, preprocess):
             coord_x_list.append(index)
             coord_y_list.append(round(float(data_list[log_data['data']]), 3))
 
-        if preprocess:
-            coord_x_list, coord_y_list = preprocess(coord_x_list, coord_y_list, 100)
-        draw_graph(coord_x_list, coord_y_list, log_data['range'], log_data['name'])
+        coord_x_list1, coord_y_list1 = preprocess(coord_x_list, coord_y_list, 100)
+        coord_x_lists.append(coord_x_list1)
+        coord_y_lists.append(coord_y_list1)
+        coord_x_list2, coord_y_list2 = preprocess(coord_x_list, coord_y_list, 1000)
+        coord_x_lists.append(coord_x_list2)
+        coord_y_lists.append(coord_y_list2)
+        draw_graph(coord_x_lists, coord_y_lists, log_data['range'])
 
 
-def draw_graph(coord_x_list, coord_y_list, range=(0.0, 1.0), y_name='data'):
-    plt.xlabel('time')  # x轴上的名字
-    plt.ylabel(y_name)  # y轴上的名字
+def draw_graph(coord_x_lists, coord_y_lists, range=(0.0, 1.0), y_name='data'):
+    import numpy
+    numpy.random.seed(9)
 
-    plt.ylim(range)
+    for index, pack in enumerate(zip(coord_x_lists, coord_y_lists)):
+        coord_x_list, coord_y_list = pack
 
-    plt.plot(coord_x_list, coord_y_list, color='orange')
+        plt.xlabel('time')  # x轴上的名字
+        plt.ylabel(y_name)  # y轴上的名字
+
+        plt.ylim(range)
+
+        r = numpy.random.rand()
+        g = numpy.random.rand()
+        b = 1 - r
+        line_color = (r, g, b)
+        plt.plot(coord_x_list, coord_y_list, color=line_color, label=str(index))
+
     plt.show()
 
-
 if __name__ == "__main__":
-    read_files(dege_log_data, preprocess)
+    read_files(dege_log_data)
