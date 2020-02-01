0e4971cc5d1ef86262a7eace9491f29ef9c75e4f
suyesheng <1245035229@qq.com>
2020-02-01 16:26:18 +0800

    optimise flist

--- scripts/flist.py
+++ scripts/flist.py
@@ -9,16 +9,16 @@ args = parser.parse_args()
 
 ext = {'.JPG', '.JPEG', '.PNG', '.TIF', 'TIFF'}
 
-flist_dataset_paths = [
-    (r"../datasets/places2_train.flist", r"E:\DeepLearnSample\EdgeConnect\CelebA\train"),
-    (r"../datasets/places2_val.flist",   r"E:\DeepLearnSample\EdgeConnect\CelebA\eval"),
-    (r"../datasets/places2_test.flist",  r"E:\DeepLearnSample\EdgeConnect\CelebA\test"),
-    (r"../datasets/masks_train.flist", r"E:\DeepLearnSample\EdgeConnect\mask\CustomMask3\train"),
-    (r"../datasets/masks_val.flist",   r"E:\DeepLearnSample\EdgeConnect\mask\CustomMask3\eval"),
-    (r"../datasets/masks_test.flist",  r"E:\DeepLearnSample\EdgeConnect\mask\CustomMask2\test"),
-]
+flist_dataset_paths = {
+    r"../datasets/places2_train.flist": [r"E:\DeepLearningData\EdgeConnect\CelebA\train",],
+    r"../datasets/places2_val.flist"  : [r"E:\DeepLearningData\EdgeConnect\CelebA\eval",],
+    r"../datasets/places2_test.flist" : [r"E:\DeepLearningData\EdgeConnect\CelebA\test",],
+    r"../datasets/masks_train.flist"  : [r"E:\DeepLearningData\EdgeConnect\mask\CustomMask3\train",],
+    r"../datasets/masks_val.flist"    : [r"E:\DeepLearningData\EdgeConnect\mask\CustomMask3\eval",],
+    r"../datasets/masks_test.flist"   : [r"E:\DeepLearningData\EdgeConnect\mask\CustomMask3\test",],
+}
 
-for flist_path, dataset_path in flist_dataset_paths:
+for flist_path, dataset_paths in flist_dataset_paths.items():
     print('creating', flist_path)
     images = []
 
@@ -26,12 +26,13 @@ for flist_path, dataset_path in flist_dataset_paths:
     os.makedirs(os.path.dirname(flist_path), exist_ok=True)
 
     # find all image
-    for root, dirs, files in os.walk(dataset_path):
-        print('loading : ' + root)
-        for file in files:
-            # print('image : ' + file)
-            if os.path.splitext(file)[1].upper() in ext:
-                images.append(os.path.join(root, file))
+    for dataset_path in dataset_paths:
+        for root, dirs, files in os.walk(dataset_path):
+            print('loading : ' + root)
+            for file in files:
+                # print('image : ' + file)
+                if os.path.splitext(file)[1].upper() in ext:
+                    images.append(os.path.join(root, file))
 
     images = sorted(images)
     np.savetxt(flist_path, images, fmt='%s')
